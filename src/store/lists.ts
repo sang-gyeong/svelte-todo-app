fc728940cc3e04b4db0be1554a7e5288a72d7527
summer(이상경) <summer.0916@kakaocommerce.com>
2022-07-01 22:24:23 +0900

    feat: list 스토어 커스텀 (add, delete 리턴함수 생성)

--- src/store/lists.ts
+++ src/store/lists.ts
@@ -1,3 +1,15 @@
 import { writable } from 'svelte/store';
+import { filter } from 'lodash-es';
 
-export const lists = writable([]);
+function createList() {
+  const { subscribe, update } = writable([]);
+
+  return {
+    subscribe,
+    add: list => update(lists => [...lists, list]),
+    delete: listId =>
+      update(lists => filter(lists, list => list.id !== listId)),
+  };
+}
+
+export const lists = createList();
