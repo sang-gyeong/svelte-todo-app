b26e0779e15df45d672de777fccfc30d8906ca47
summer(이상경) <summer.0916@kakaocommerce.com>
2022-07-03 16:28:37 +0900

    feat: 서버 응답값에 대응하는 인터페이스, 타입별칭은 d.ts 파일로 분리(namespace)

---
+++ src/models/Card.d.ts
@@ -0,0 +1,10 @@
+declare namespace Card {
+  interface Item {
+    order: number;
+    id: string;
+    listId: string;
+    color?: string;
+    content: string;
+    createdAt: string;
+  }
+}
---
+++ src/models/List.d.ts
@@ -0,0 +1,9 @@
+declare namespace List {
+  interface Item {
+    order?: number;
+    id: string;
+    color?: string;
+    title: string;
+    createdAt: Date;
+  }
+}
