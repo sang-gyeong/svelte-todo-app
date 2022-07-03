6e8e0873a0e30c1cfc69e6457298b30c2ba4ec55
summer(이상경) <summer.0916@kakaocommerce.com>
2022-07-03 16:27:57 +0900

    feat: 배경 색상에 따라 흰/검은색 리턴하는 유틸함수 생성

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
 export function createRandom(): string {
   return Math.random().toString(36).substring(2, 11);
 }
+
+export function getColorByBgColor(bgColor: string) {
+  if (!bgColor) {
+    return '#fff';
+  }
+  return parseInt(bgColor.replace('#', ''), 16) > 0xffffff / 2
+    ? '#000'
+    : '#fff';
+}
