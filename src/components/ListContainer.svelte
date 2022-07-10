ce4e5ce2ce33ed503b7f070993744ca0cd06a64e
summer(이상경) <summer.0916@kakaocommerce.com>
2022-07-10 13:43:20 +0900

    feat: dragAndDrop 디렉티브 (action) 생성 및 적용

--- src/components/ListContainer.svelte
+++ src/components/ListContainer.svelte
@@ -2,19 +2,37 @@
   import ListCreater from './ListCreater.svelte';
   import List from './List.svelte';
   import { lists } from '../store/lists';
+  import { dragAndDrop } from '../actions/dragAndDrop';
+  import { onMount } from 'svelte';
+  import * as listService from '../api/list';
+
+  onMount(() => {
+    listService.loadLists().then(({ data }) => {
+      lists.init(data);
+    });
+  });
 </script>
 
-<div class="list-container">
-  {#each $lists as { id: listId, title, color: listColor } (listId)}
-    <List {listId} {title} {listColor} />
-  {/each}
+<div class="wrapper">
+  {#if $lists.length}
+    <div class="list-container" use:dragAndDrop>
+      {#each $lists as { id: listId, title, color: listColor } (listId)}
+        <List {listId} {title} {listColor} />
+      {/each}
+    </div>
+  {/if}
   <ListCreater />
 </div>
 
 <style>
-  .list-container {
-    width: 100%;
+  .wrapper {
+    width: fit-content;
     min-height: calc(100% - 50px);
+    display: flex;
+    gap: 50px;
+  }
+
+  .list-container {
     display: flex;
     gap: 50px;
   }
